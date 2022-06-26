292b29847fe17cbbb8885878230148f72fdb51f3
LDY <liashkod@gmail.com>
2022-06-26 13:40:54 +0200

    CLeaned and moved custom stores to DAL

--- BlazorAppAuthTest/CustomAuntAuth/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿// TODO is custom role is needed
-//namespace BlazorAppAuthTest.CustomAuntAuth
-//{
-//    public class ApplicationRole
-//    {
-//        public Guid Id { get; set; } = Guid.NewGuid();
-//        public string Name { get; set; }
-//    }
-//}
