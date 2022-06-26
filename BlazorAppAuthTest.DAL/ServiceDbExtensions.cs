6294516699d6d1f15bedebf67ed57541b68e05c1
LDY <liashkod@gmail.com>
2022-06-26 14:09:36 +0200

    Added Extensions

---
+++ BlazorAppAuthTest.DAL/ServiceDbExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BlazorAppAuthTest.DAL.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorAppAuthTest.DAL
+{
+    public static class ServiceDbExtensions
+    {
+        public static void AddCustomUserRoleStores(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IUserRepository, LiteDbUserRepository>();
+
+            serviceCollection.AddSingleton<ILiteDbRepository, LiteDbRepository>();
+            serviceCollection.AddSingleton<IRoleRepository, LiteDbRoleRepository>();
+        }
+    }
+}
