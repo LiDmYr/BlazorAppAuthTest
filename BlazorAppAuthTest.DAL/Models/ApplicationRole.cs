17bec96d6a19d9c69ba85424ed3b205ceaaa7e8e
LDY <liashkod@gmail.com>
2022-06-26 14:49:05 +0200

    Cleaned

--- BlazorAppAuthTest.DAL/Models/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace BlazorAppAuthTest.DAL.Models
-//{
-//    public class ApplicationRole
-//    {
-//        public Guid Id { get; set; } = Guid.NewGuid();
-//        public string Name { get; set; }
-//    }
-//}
